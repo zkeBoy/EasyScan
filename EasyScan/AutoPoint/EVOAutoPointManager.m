ef9c1c08ce95265adff8b190c26f2945fd060e18
zkeBoy <985777468@qq.com>
2020-05-12 18:26:42 +0800

    Fix:更新计数规则

--- EasyScan/AutoPoint/EVOAutoPointManager.m
+++ EasyScan/AutoPoint/EVOAutoPointManager.m
@@ -21,14 +21,76 @@
 //重置随机点
 - (void)resetAutoPoint:(NSInteger)number {
     [self.pointerArray removeAllObjects];
-    for (NSInteger idx = 0; idx<number; idx++) {
-        EVOPointerObj * obj = [EVOPointerObj new];
-        //获取一个随机整数范围在：[0,100)包括0，不包括100
-        int x = arc4random() % 200-100;
-        int y = arc4random() % 150-50;
-        obj.x = x;
-        obj.y = y;
-        [self.pointerArray addObject:obj];
+    if (number>10) {
+        for (NSInteger idx = 0; idx<10; idx++) {
+            EVOPointerObj * obj = [EVOPointerObj new];
+            //获取一个随机整数范围在：[0,100)包括0，不包括100
+            int x = arc4random() % 100-100;
+            int y = arc4random() % 150-50;
+            obj.x = x;
+            obj.y = y;
+            [self.pointerArray addObject:obj];
+        }
+    }
+    
+    if (number>20) {
+        for (NSInteger idx = 10; idx<20; idx++) {
+            EVOPointerObj * obj = [EVOPointerObj new];
+            //获取一个随机整数范围在：[0,100)包括0，不包括100
+            int x = arc4random() % 100;
+            int y = arc4random() % 150;
+            obj.x = x;
+            obj.y = y;
+            [self.pointerArray addObject:obj];
+        }
+    }
+    
+    if (number>30) {
+        for (NSInteger idx = 20; idx<30; idx++) {
+            EVOPointerObj * obj = [EVOPointerObj new];
+            //获取一个随机整数范围在：[0,100)包括0，不包括100
+            int x = arc4random() % 100-50;
+            int y = arc4random() % 100-50;
+            obj.x = x;
+            obj.y = y;
+            [self.pointerArray addObject:obj];
+        }
+    }
+    
+    if (number>40) {
+        for (NSInteger idx = 30; idx<40; idx++) {
+            EVOPointerObj * obj = [EVOPointerObj new];
+            //获取一个随机整数范围在：[0,100)包括0，不包括100
+            int x = arc4random() % 200-50;
+            int y = arc4random() % 100;
+            obj.x = x;
+            obj.y = y;
+            [self.pointerArray addObject:obj];
+        }
+    }
+    
+    if (number>50) {
+        for (NSInteger idx = 40; idx<50; idx++) {
+            EVOPointerObj * obj = [EVOPointerObj new];
+            //获取一个随机整数范围在：[0,100)包括0，不包括100
+            int x = arc4random() % 150;
+            int y = arc4random() % 200;
+            obj.x = x;
+            obj.y = y;
+            [self.pointerArray addObject:obj];
+        }
+    }
+    
+    if (number>60) {
+        for (NSInteger idx = 50; idx<number; idx++) {
+            EVOPointerObj * obj = [EVOPointerObj new];
+            //获取一个随机整数范围在：[0,100)包括0，不包括100
+            int x = arc4random() % 250-100;
+            int y = arc4random() % 150-50;
+            obj.x = x;
+            obj.y = y;
+            [self.pointerArray addObject:obj];
+        }
     }
 }
 
